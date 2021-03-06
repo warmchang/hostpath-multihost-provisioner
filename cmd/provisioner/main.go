0e5c64575ff3102a4bbeb80bc2824203f3947214
Adrián Gallego Castellanos <kugoad@openmailbox.org>
2021-03-06 02:37:02 +0100

    Added comments to provisioner code. Refactor provisioner code.

--- cmd/provisioner/main.go
+++ cmd/provisioner/main.go
@@ -26,6 +26,7 @@ import (
     "net/http"
     "net/url"
     "fmt"
+    "strconv"
 
 	"github.com/golang/glog"
 	"sigs.k8s.io/sig-storage-lib-external-provisioner/v6/controller"
@@ -69,17 +70,27 @@ func NewHostPathProvisioner() controller.Provisioner {
 
 var _ controller.Provisioner = &hostPathProvisioner{}
 
-// Provision creates a storage asset and returns a PV object representing it.
+// Provision sends a request to every manager to create a storage asset in every node and returns a PV object representing it.
 func (p *hostPathProvisioner) Provision(_ context.Context, options controller.ProvisionOptions) (*v1.PersistentVolume, controller.ProvisioningState, error) {
+    // Compute path in the manager pods where persistent volumes are going to be created.
 	path := path.Join(pvDir, options.PVC.Namespace+"-"+options.PVC.Name+"-"+options.PVName)
 	glog.Infof("Creating backing directory: %v", path)
-    sendRequestToManager(path, createDir)
 
+    // Send a creation request of the computed path to every manager pod.
+    // Manager runs as DaemonSet. So this path is going to be created on every node.
+    err := sendRequestToManager(path, createDir)
+    if err != nil {
+        return nil, controller.ProvisioningFinished, err
+    }
+
+    // If PV_RECLAIM_POLICY is defined, then, use that policy as the policy of every created node.
+    // Otherwise, use the storage class default policy.
 	reclaimPolicy := *options.StorageClass.ReclaimPolicy
 	if p.reclaimPolicy != "" {
 		reclaimPolicy = v1.PersistentVolumeReclaimPolicy(p.reclaimPolicy)
 	}
 
+    // Create the new persistent volume with the computed path and policy.
 	pv := &v1.PersistentVolume{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: options.PVName,
@@ -101,32 +112,47 @@ func (p *hostPathProvisioner) Provision(_ context.Context, options controller.Pr
 		},
 	}
 
+    // Return the created persistent volume successfully.
 	return pv, controller.ProvisioningFinished, nil
 }
 
 
-type HttpStatusError struct {
+// This struct represents and http status error. Used to return error when status is not 200 OK.
+type httpStatusError struct {
     status int
 }
 
-func (e HttpStatusError) Error() string {
+func (e httpStatusError) Error() string {
     return fmt.Sprintf("HTTP Status Error with status code: %v", e.status)
 }
 
-func sendRequestToManager(path string, requestFunc func(chan error, string, string)) error {
+// A function which performs a request agains the managers rest API.
+// Providing the ip of the manager, and the filesystem path of the object to manage.
+// It returns an error because the function can fail if the reques fails.
+type managerRequestFunction func(ip string, path string) error
+
+// It sends a request to every manager monitored by the manager service.
+// The requests are sent in parallel to every manager pod.
+// It returns an error if any of the request fails.
+func sendRequestToManager(path string, requestFunc managerRequestFunction) error {
+    // Resolv every DNS behind headless service for manager.
     glog.Infof("Looking for service %q.", storageManagerServiceName)
     ips, err := net.LookupHost(storageManagerServiceName)
     if err != nil {
-        glog.Fatalf("Error looking for service: %q", err.Error())
+        glog.Errorf("Error looking for service: %q", err.Error())
         return err
     }
 
+    // Perform a request in parallel to every manager monitored by the manager service.
     glog.Infof("Start sending requests.")
     results := make(chan error)
     for _, ip := range ips {
-        go requestFunc(results, ip, path)
+        go func() {
+            results <- requestFunc(ip, path)
+        }()
     }
 
+    // Wait for every request to finish and return error if any fail.
     for range ips {
         err := <- results    
         if err != nil {
@@ -137,53 +163,57 @@ func sendRequestToManager(path string, requestFunc func(chan error, string, stri
     return nil
 }
 
-func createDir(results chan error, ip string, path string) {
-    glog.Infof("Sending to ip: %q to path %q", ip, path)
+// Send a POST request to create a directory at the given filesystem path to the provided ip address.
+// It returns an error if there is any problem sending the request.
+func createDir(ip string, path string) error {
     targetUrl := fmt.Sprintf("http://%v:%v/directories", ip, storageManagerServicePort)
-    glog.Infof("Request sent to %q.", targetUrl)
 
+    // Send the creation request to manager.
+    glog.Infof("Sending POST request to %q, with path %q.", targetUrl, path)
     resp, err := http.PostForm(targetUrl, url.Values{"path": {path}})
     if err != nil {
-        results <- err
-        return
+        return err
     }
 
+    // Ensure to close the response body at the end.
     defer resp.Body.Close()
     
+    // If the status code is not successfull return an httpStatusError.
     if resp.StatusCode != http.StatusOK {
-        results <- HttpStatusError{resp.StatusCode}
-        return
+        return httpStatusError{resp.StatusCode}
     }
 
-    results <- nil
+    return nil
 }
 
-func deleteDir(results chan error, ip string, path string) {
-    glog.Infof("Sending to ip: %q and path %q", ip, path)
+// Send a DELETE request to remove a directory at the given filesystem path to the provided ip address.
+// It returns an error if there is any problem sending the request.
+func deleteDir(ip string, path string) error {
     targetUrl := fmt.Sprintf("http://%v:%v/directories?path=%v", ip, storageManagerServicePort, path)
-    glog.Infof("Request sent to %q.", targetUrl)
 
+    // Create DELETE request.
+    glog.Infof("Sending DELETE request to %q, with path %q.", targetUrl, path)
     req, err := http.NewRequest(http.MethodDelete, targetUrl, nil)
     if err != nil {
-        results <- err
-        return
+        return err
     }
 
+    // Send DELETE request to manager.
     client := &http.Client{}
     resp, err := client.Do(req)
     if err != nil {
-        results <- err
-        return
+        return err
     }
 
+    // Ensure to close the response body at the end.
     defer resp.Body.Close()
     
+    // If the status code is not successfull return an httpStatusError.
     if resp.StatusCode != http.StatusOK {
-        results <- HttpStatusError{resp.StatusCode}
-        return
+        return httpStatusError{resp.StatusCode}
     }
 
-    results <- nil
+    return nil
 }
 
 
@@ -191,6 +221,7 @@ func deleteDir(results chan error, ip string, path string) {
 // Delete removes the storage asset that was created by Provision represented
 // by the given PV.
 func (p *hostPathProvisioner) Delete(_ context.Context, volume *v1.PersistentVolume) error {
+    // Check that the deleted volume is managed by this provisioner. Otherwise, ignore it.
 	ann, ok := volume.Annotations["hostPathMultiHostProvisionerIdentity"]
 	if !ok {
 		return errors.New("identity annotation not found on PV")
@@ -199,12 +230,16 @@ func (p *hostPathProvisioner) Delete(_ context.Context, volume *v1.PersistentVol
 		return &controller.IgnoredError{Reason: "identity annotation on PV does not match ours"}
 	}
 
+    // Check the reclaim policy of the deleted volume. If retain do not delete the contents
+    // of the volume.
     onDelete := volume.Spec.PersistentVolumeReclaimPolicy
     if onDelete == "Retain" {
 	    glog.Infof("Not removing backing directory because policy is Retain.")
         return nil
     }
 
+    // If reclaim policy is not retain, then, delete send delete to remove the volume in
+    // every manager pod. This will delete the contents of this volume in every node.
 	path := volume.Spec.PersistentVolumeSource.HostPath.Path
 	glog.Info("Removing backing directory: %v", path)
     sendRequestToManager(path, deleteDir)
@@ -236,6 +271,16 @@ func main() {
 		glog.Fatalf("Error getting server version: %v", err)
 	}
 
+    // Allow to enable or disable leader election using environment variable ENABLE_LEADER_ELECTION.
+    leaderElection := true
+	leaderElectionEnv := os.Getenv("ENABLE_LEADER_ELECTION")
+	if leaderElectionEnv != "" {
+		leaderElection, err = strconv.ParseBool(leaderElectionEnv)
+		if err != nil {
+			glog.Fatalf("Unable to parse ENABLE_LEADER_ELECTION env var: %v", err)
+		}
+	}
+
 	// Create the provisioner: it implements the Provisioner interface expected by
 	// the controller
 	hostPathProvisioner := NewHostPathProvisioner()
@@ -246,8 +291,9 @@ func main() {
         provisionerName, 
         hostPathProvisioner,
         serverVersion.GitVersion,
-        controller.LeaderElection(true),
+        controller.LeaderElection(leaderElection),
     )
 
+    // Start executing the provisioner.
 	pc.Run(context.Background())
 }
